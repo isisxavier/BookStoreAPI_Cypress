dcbec196145d576d4c11a707140ba32dc7a4c938
Isis Xavier <isisxavierti@gmail.com>
2023-04-19 16:48:59 -0300

    generateToken e authorized

--- cypress/e2e/account.cy.js
+++ cypress/e2e/account.cy.js
@@ -8,82 +8,160 @@ describe('Casos de teste sobre a rota /account/v1/Users da API Book Store', () =
   
       it('Cria usuário', () => {
         cy.geraUser().then(res => {
-            cy.contractValidation(res, 'account/post_account_user', 201)
+            ValidaBookstore.validaCriarUser(res)
+            cy.salvarUserId(res)
         })
       })
     })
 
-
-  
     describe('Casos positivos de erro', () => {
-  
-      it('Não deve ser possível tentar gerar usuário ja existente', () => {
+
+      it('Deve retornar erro ao tentar gerar usuário ja existente', () => {
         cy.geraUserExistente().then(res => {
-            cy.contractValidation(res, 'account/post_account_user', 406)
-            expect(res.body.message).to.be.eq('User exists!')
+            ValidaBookstore.validaCriarUserExistente(res)
         })
       })
-
-
-
     })
-
-
 
     describe('Casos de erros não documentados', () => {
   
         it('Deve retornar erro se o campo USERNAME estiver em branco', () => {
           cy.geraUserSemUserName().then(res => {
-              cy.contractValidation(res, 'account/post_account_user_undoc', 400)
-              expect(res.body.message).to.be.eq('UserName and Password required.')
+              ValidaBookstore.validarCamposEmBranco(res)
             })
         })
         
         it('Deve retornar erro se o campo PASSWORD estiver em branco', () => {
             cy.geraUserSemSenha().then(res => {
-                cy.contractValidation(res, 'account/post_account_user_undoc', 400)
-                expect(res.body.message).to.be.eq('UserName and Password required.')
+                ValidaBookstore.validarCamposEmBranco(res)
             })
         })
+
 
         context('Validando senhas', () => {
 
             it('Deve retornar erro se a senha não possuir número', () => {
             cy.gerarSenhaSemNúmero().then(res => {
-                ValidaBookstore.validarSenhaSemNum(res)
+                ValidaBookstore.validarSenha(res)
                 })
             })
 
             it('Deve retornar erro se a senha não possuir letra maiúscula', () => {
             cy.gerarSenhaSemLetraMaiuscula().then(res => {
-                ValidaBookstore.validarSenhaSemMaiuscula(res)
+                ValidaBookstore.validarSenha(res)
                 })
             })
 
             it('Deve retornar erro se a senha não possuir letra minúscula', () => {
                 cy.gerarSenhaSemLetraMinuscula().then(res => {
-                    ValidaBookstore.validarSenhaSemMinuscula(res)
+                    ValidaBookstore.validarSenha(res)
                     })
                 })
 
             it('Deve retornar erro se a senha não possuir caractere especial', () => {
             cy.gerarSenhaSemCaractereEspc().then(res => {
-                ValidaBookstore.validarSenhaSemCaractere(res)
+                ValidaBookstore.validarSenha(res)
                 })
             })  
 
             it('Deve retornar erro se a senha for menor que 8 caracteres', () => {
             cy.gerarSenhaMenorOito().then(res => {
-                ValidaBookstore.validarSenhaMenorOito(res)
+                ValidaBookstore.validarSenha(res)
+                })
+            })
+        })  
+    })
+})
+
+describe('Casos de teste sobre a rota /account/v1/GenerateToken da API Book Store', () => {
+
+    describe('Casos positivos de sucesso.', () => {
+  
+        it('Gera token', () => {
+          cy.geraUserParaToken().then((user) => {
+              cy.geraToken(user.userName, user.password).then(res =>{
+                ValidaBookstore.validarGerarToken(res)
+              })
+          })
+        })
+    })
+
+    describe('Casos positivos de erro', () => {
+
+        it('Não deve ser possível gerar token com UserName errado', () => {
+            cy.geraTokenUserErrado().then(res =>{
+                ValidaBookstore.validarGerarTokenDadosErrados(res)
+            })
+        })
+
+        it('Não deve ser possível gerar token com Senha errada', () => {
+            cy.geraTokenSenhaErrada().then(res =>{
+                ValidaBookstore.validarGerarTokenDadosErrados(res)
+            })
+            
+        })
+
+        it('Deve retornar erro se o campo USERNAME estiver em branco', () => {
+            cy.geraTokenUserEmBranco().then(res => {
+                ValidaBookstore.validarGerarTokenDadosEmBranco(res)
+              })
+          })
+          
+        it('Deve retornar erro se o campo PASSWORD estiver em branco', () => {
+            cy.geraTokenSenhaEmBranco().then(res => {
+                ValidaBookstore.validarGerarTokenDadosEmBranco(res)
+            })
+        })
+    })
+})
+
+describe('Casos de teste sobre a rota /account/v1/Authorized da API Book Store', () => {
+
+    describe('Casos positivos de sucesso.', () => {
+  
+        it('Gera autorização', () => {
+            cy.geraUserParaToken().then((user) => {
+                cy.geraToken(user.userName, user.password).then(() =>{
+                    cy.geraAutorização(user.userName, user.password).then(res => {
+                        ValidaBookstore.validarGerarAutorizationTrue(res)
+                    })
+                })
+            })
+        })
+    })
+
+    describe('Casos positivos de erro', () => {
+
+        it('Não deve ser permitido gerar autorização para usuário sem token', () => {
+            cy.geraUserParaToken().then((user) => {
+                cy.geraAutorização(user.userName, user.password).then(res => {
+                    ValidaBookstore.validarGerarAutorizationFalse(res)
                 })
             })
         })
 
-        
-  
-  
+        it.only('Deve retornar erro se o campo USERNAME estiver em branco', () => {
+            cy.geraUserSemUserName().then((user) => {
+                cy.geraAutorização(user.userName, user.password).then(res => {
+                    // ValidaBookstore.validarGerarAutorizationFalse(res)
+                })
+            })
+        })
+
+        it.only('Deve retornar erro se o campo PASSWORD estiver em branco', () => {
+            cy.geraUserParaToken().then((user) => {
+                cy.geraAutorização(user.userName, user.password).then(res => {
+                    ValidaBookstore.validarGerarAutorizationFalse(res)
+                })
+            })
+        }) 
+
+        it('Deve retornar erro se tentar gerar autorização para usuário inexistente', () => {
+            cy.geraUserParaToken().then((user) => {
+                cy.geraAutorização(user.userName, user.password).then(res => {
+                    ValidaBookstore.validarGerarAutorizationFalse(res)
+                })
+            })
+        }) 
     })
-  
-  
-  
-  })
+})
--- cypress/services/validacao.js
+++ cypress/services/validacao.js
@@ -1,29 +1,56 @@
 
 export default class ValidaBookstore {
 
-    static validarSenhaSemNum(res){
-        cy.contractValidation(res, 'account/post_account_user_undoc2', 400)
+    /*----------- ACCOUNT/USERS ------------- */
+
+    static validaCriarUser(res){
+        cy.contractValidation(res, 'account/users/post-user', 201)
+    }
+
+    static validaCriarUserExistente(res){
+        cy.contractValidation(res, 'account/users/post-user', 406)
+        expect(res.body.message).to.be.eq('User exists!')
+    }
+
+    static validarCamposEmBranco(res){
+        cy.contractValidation(res, 'account/users/post-user-undoc', 400)
+        expect(res.body.message).to.be.eq('UserName and Password required.')
+    }
+
+    static validarSenha(res){
+        cy.contractValidation(res, 'account/users/post-user-undoc2', 400)
         expect(res.body.message).to.be.eq("Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.")
     }
 
-    static validarSenhaSemMaiuscula(res){
-        cy.contractValidation(res, 'account/post_account_user_undoc2', 400)
-        expect(res.body.message).to.be.eq("Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.")
+    /*----------- ACCOUNT/GENERATETOKEN ------------- */
+
+    static validarGerarToken(res){
+        cy.contractValidation(res, 'account/generateToken/post-GenerateToken', 200)
+        expect(res.body.status).to.be.eq("Success")
+        expect(res.body.result).to.be.eq("User authorized successfully.")
     }
 
-    static validarSenhaSemMinuscula(res){
-        cy.contractValidation(res, 'account/post_account_user_undoc2', 400)
-        expect(res.body.message).to.be.eq("Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.")
+    static validarGerarTokenDadosErrados(res){
+        cy.contractValidation(res, 'account/generateToken/post-GenerateToken-failed', 200)
+        expect(res.body.status).to.be.eq("Failed")
+        expect(res.body.result).to.be.eq("User authorization failed.")
     }
 
-    static validarSenhaSemCaractere(res){
-        cy.contractValidation(res, 'account/post_account_user_undoc2', 400)
-        expect(res.body.message).to.be.eq("Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.")
+    static validarGerarTokenDadosEmBranco(res){
+        cy.contractValidation(res, 'account/generateToken/post-GenerateToken', 400)
+        expect(res.body.message).to.be.eq('UserName and Password required.')
     }
 
-    static validarSenhaMenorOito(res){
-        cy.contractValidation(res, 'account/post_account_user_undoc2', 400)
-        expect(res.body.message).to.be.eq("Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.")
+    /*----------- ACCOUNT/AUTHORIZATION ------------- */
+
+    static validarGerarAutorizationTrue(res){
+        cy.contractValidation(res, 'account/authorization/post-authorization', 200)
+        expect(res.body).to.be.true;
+    }
+
+    static validarGerarAutorizationFalse(res){
+        cy.contractValidation(res, 'account/authorization/post-authorization', 200)
+        expect(res.body).to.be.false;
     }
 
 }
